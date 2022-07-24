6975771116131bf8fae7883a3a25ed2bb2ed088c
marcelo amorim <marcelo.med.amorim@gmail.com>
2022-07-24 12:45:27 -0300

    doc: adicionada documentação

--- cadastro-usuario/src/main/java/br/com/cadastrousuario/controller/UsuarioController.java
+++ cadastro-usuario/src/main/java/br/com/cadastrousuario/controller/UsuarioController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * RestController: Anotação do Spring responsável por indicar classe controller
+ * RequestMapping: Anotação do Spring responsável por mapear o caminho (path) da requisição
+ */
 @RestController
 @RequestMapping("/v1/usuarios")
 public class UsuarioController {
 
     /**
      * [modificador de acesso] [retorno da função] [nome da função]
+     * GetMapping: Anotação do Spring responsável por definir qual verbo HTTP será utilizado na requisição
      */
     @GetMapping
     public String criarUsuario() {
